3fe8df6bd406c2bcb9d6fd47d4d9feb64afde0b1
farhanahmedanik59 <anikianiki21@gmail.com>
2025-12-05 23:53:55 +0600

    top scholarships

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+var cors = require("cors");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.gq2vs5u.mongodb.net/?appName=Cluster1`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const db = client.db("scholarlink");
+    const scholarshipsCollection = db.collection("scholarships");
+
+    // scholarships api
+    app.get("/scholarships/top", async (req, res) => {
+      const result = await scholarshipsCollection.find().sort({ createdAt: -1 }).limit(6).toArray();
+      res.send(result);
+    });
+
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
